28ea6538f4eb186942d41d17ba7a8d5c3f958cac
wannatshirtfame <vladyslav.rybalka@gmail.com>
2015-04-08 15:42:41 +0300

    Initial stage of Exflame development

---
+++ www/js/contactform.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+ 
+	$(#ajax-contact-form»).submit(function() {
+	var str = $(this).serialize();
+	 
+	$.ajax({
+		type: "POST",
+		url: "contact.php",
+		data: str,
+		success: function(msg) {
+	 
+			if(msg == 'OK') {
+				result = 'Ваше сообщение было отправлено';
+			$('#fields').hide();
+			} else {
+				result = msg;
+			}
+
+			$('#note').html(result);
+		}
+	});
+
+	return false;
+	
+	});
+});
