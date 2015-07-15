4dac26a160bf6ea1ac5a672968506259f3f1ff67
Vladyslav Rybalka <vladyslav.rybalka@gmail.com>
2015-07-15 10:01:33 +0300

    Update contact.php

--- www/php/contact.php
+++ www/php/contact.php
@@ -8,7 +8,7 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$admin_email = 'exflame.studio@gmail.com'; // Your Email
+$admin_email = '#'; // Your Email
 $message_min_length = 5; // Min Message Length
 
 
